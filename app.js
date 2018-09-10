4f1cb31c0ae3b06994b962179940a1797a869f76
Selma Delgado <selmadelgado@gmail.com>
2018-09-09 21:45:47 -0500

    Structure Setup with Node Modules

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_bZW5BqNybsg5V1bHDoij1xyz');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
