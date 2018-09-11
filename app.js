a1641527c81635260afe330412da4cefc59b90bf
Selma Delgado <selmadelgado@gmail.com>
2018-09-11 14:55:10 -0500

    Added Default Node Packages

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Public key: pk_test_drOZ8fjels9TRp9id5EdnO0Y
+// Secret key: sk_test_bZW5BqNybsg5V1bHDoij1xyz
+
 const express = require('express');
 const stripe = require('stripe')('sk_test_bZW5BqNybsg5V1bHDoij1xyz');
 const bodyParser = require('body-parser');
@@ -5,6 +8,13 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+/*
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -21,8 +31,4 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+*/
